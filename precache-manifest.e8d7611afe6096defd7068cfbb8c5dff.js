d5ad4bd37b34e0ac554271d9a7f1f3804f9e7f38
wua3 <aileenwu24601@gmail.com>
2019-08-31 15:12:01 -0400

    Updates

---
+++ precache-manifest.e8d7611afe6096defd7068cfbb8c5dff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a995c2fd7c3bd74c39fdce11fe78e3c3",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "09c7884cd8c2606c414a",
+    "url": "/pomodoro/static/css/main.611acfdd.chunk.css"
+  },
+  {
+    "revision": "5be378b9f26ef3b94ad1",
+    "url": "/pomodoro/static/js/2.87b711be.chunk.js"
+  },
+  {
+    "revision": "09c7884cd8c2606c414a",
+    "url": "/pomodoro/static/js/main.9e86eb7b.chunk.js"
+  },
+  {
+    "revision": "fbbc4112b8f48e9daeb4",
+    "url": "/pomodoro/static/js/runtime~main.e2c3b59c.js"
+  }
+]);
